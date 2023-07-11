dd1c4ec00e95ecbd9a44b2c82023ce26e666f7c0
Glory Gupta <prikarshglory@gmail.com>
2023-07-11 16:25:00 +0530

    Create Solution.cpp

---
+++ Find minimum in rotated sorted II /Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, high = nums.size()-1;
+        while(low<high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] > nums[high]){
+                low = mid + 1;
+            }
+            else if(nums[mid] < nums[high])
+                high = mid;
+            else if(nums[high] == nums[high-1])
+            high--;
+            else
+            low++;
+        }
+        return nums[low];
+    }
+};
